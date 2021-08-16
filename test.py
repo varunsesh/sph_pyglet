0d9f77df09ed3a4106c0c5c938233988c9c127bc
Varun <varunsesh@gmail.com>
2021-08-17 00:00:29 +0530

    Viewer demo; moving circle, control the rectangle

--- test.py
+++ test.py
@@ -1,35 +1,50 @@
 import pyglet
-from pyglet.window import key
-from pyglet.window import mouse
+from pyglet.window import key, mouse
 from pyglet import shapes
 
 
-
+x = 250
 window = pyglet.window.Window()
 batch = pyglet.graphics.Batch()
-
+rectangle = shapes.Rectangle(x, 300, 400, 200, color=(255, 22, 20), batch=batch)
+pos_x = 20
+pos_y = window.height//2
+speed = 100.0
 
 
 @window.event
 def on_key_press(symbol, modifiers):
-    if symbol==key.A:
-        print("Key A was pressed")
-    elif symbol==key.ENTER:
-        print("Key Enter was pressed")
-    
-r = 100   
-circle1=shapes.Circle(320,320, r, color=(50, 225, 30), batch=batch)
-circle1.opacity = 120
+    if symbol == key.LEFT:
+        rectangle.x -= 10
+    elif symbol == key.RIGHT:
+        rectangle.x += 10
+    elif symbol == key.UP:
+        rectangle.y += 10
+    elif symbol == key.DOWN:
+        rectangle.y -= 10
+
+
+
+
 
 @window.event
 def on_draw():
     window.clear()
+    circle = shapes.Circle(pos_x, pos_y, 20, color=(50, 225, 30), batch=batch)
     batch.draw()
 
 
+def update(dt):
+    global pos_x
+    global pos_y
+    pos_x += dt*speed
+    pos_y += dt*speed
+    print(pos_x, pos_y)
+    
+#event_logger = pyglet.window.event.WindowEventLogger()
+#window.push_handlers(event_logger)
 
-event_logger = pyglet.window.event.WindowEventLogger()
-window.push_handlers(event_logger)
 
+pyglet.clock.schedule_interval(update, 1/60)
 
 pyglet.app.run()
