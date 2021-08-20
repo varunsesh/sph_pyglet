63cc5bd54206ba175368fa4fded178858cfe7d96
Varun <varunsesh@gmail.com>
2021-08-20 10:24:07 +0530

    Added function to compute pressure, untested

--- main/Solver.py
+++ main/Solver.py
@@ -4,13 +4,20 @@ import math
 
 
 class Solver():
-    def __init__(self):
+    
+
+    def __init__(self, H, rho0, k):
         self.pm = ParticleManager()
         self.deltaT = 0.01
+        self.H = H
+        self.rho0 = rho0
+        self.k = k
+        self.HSQ = self.H*self.H
+        self.__kernel_constant = 315/(1*math.pi*pow(H, 9.0))
 
 
 
-    def InitSPH(self, Re, height):
+    def init_sph(self, Re, height):
         number_particles = 16
         
         for y in range(0,height):
@@ -20,14 +27,24 @@ class Solver():
         return self.pm.particleList
 
 
-
-
-    def ComputeForces(self):
+    def compute_forces(self):
         F = Vector2D(100.0,100.0)
         return F
 
-    def UpdatePos(self):
-        F = self.ComputeForces()
+    ## Using Poly6 Kernel Function
+    def compute_density_pressure(self):
+        r = Vector2D(0.0, 0.0)
+        for pi in self.pm.particleList:
+            pi.rho = 0.0
+            for pj in self.pm.particleList:
+                r2 = (r.distance_to(pj.position, pi.position))**2
+                if (r2<self.HSQ):
+                    pi.rho += 1*self.__kernel_constant*(self.HSQ-r2)**3
+            pi.p = self.k*pi.rho - self.rho0
+
+
+    def update_pos(self):
+        F = self.compute_forces()
         for p in self.pm.particleList:
             p.position = p.position +(p.velocity+F*self.deltaT)*self.deltaT
         
